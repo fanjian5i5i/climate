3a04a20f458cb2929be80872151ee869bb8f50dc
fjgispc <jian.fan@boston.gov>
2017-11-03 16:25:47 -0700

    working sample

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const css = require('./app.scss');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const App = () => (
+  <MuiThemeProvider>
+    <RaisedButton label="Default" />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
